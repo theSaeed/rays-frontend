1531a5cadc134103062eefb8e9d5ef1a5920c5f1
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2022-01-22 16:32:35 +0330

    Anchor links are back! + login and signup btn hide-ability.

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import '../stylesheets/titlebar.css';
+import '../App.css';
 import logo from '../media/logo-no-shadow.png';
 import { Link } from 'react-router-dom';
 
 export const TitleBar = () => {
+
+    const isLoginButtonHidden = () => {
+        if (window.location.pathname === '/login')
+            return ' hidden';
+        return '';
+    }
+
+    const isSignupButtonHidden = () => {
+        if (window.location.pathname === '/signup')
+            return ' hidden';
+        return '';
+    }
+
     return (
         <div className='titlebar'>
             <div className='flexbox-container'>
-                <Link className='title-img-link' to='/'><img className='title-img' src={logo} /></Link>
+                <a className='title-img-link' href='/'><img className='title-img' src={logo} alt='Logo'/></a>
                 <div className='title-text-container'>
-                    <Link className='title-text-link' to='/'><h3 className='title-text'>Rays</h3></Link>
+                    <a className='title-text-link' href='/'><h3 className='title-text'>Rays</h3></a>
                 </div>
-                <div className='title-login-container'>
-                    <Link className='title-login' to='/login'><p className='title-login-text'>Log in</p></Link>
+                <div className={'title-login-container'+isLoginButtonHidden()}>
+                    <a className='title-login' href='/login'><p className='title-login-text'>Log in</p></a>
                 </div>
-                <div className='title-signup-container'>
-                    <Link className='title-signup' to='/signup'><p className='title-signup-text'>Sign up</p></Link>
+                <div className={'title-signup-container'+isSignupButtonHidden()}>
+                    <a className='title-signup' href='/signup'><p className='title-signup-text'>Sign up</p></a>
                 </div>
             </div>
         </div>
